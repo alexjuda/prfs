050f05f354a18e10df8aed31960d453c57a1754a
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2026-03-15 23:14:33 +0100

    ci: tests & publish workflows (#4)

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,12 +1,19 @@
+import os
 import subprocess
 
 
 class TestHelp:
     @staticmethod
     def test_top_level():
-        proc = subprocess.run(["prfs"], capture_output=True, text=True)
-        assert "Usage: prfs" in proc.stdout
-        assert "--help" in proc.stdout
+        proc = subprocess.run(
+            ["prfs"],
+            capture_output=True,
+            text=True,
+            # Colored output screws up assertions on the CI because it detects there's no TTY.
+            env={**os.environ, "NO_COLOR": "1"},
+        )
+        assert "Usage:" in proc.stdout
+        assert "prfs" in proc.stdout
         assert "fetch" in proc.stdout
         assert "clean" in proc.stdout
 
